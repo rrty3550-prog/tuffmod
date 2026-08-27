8b9f0533ed2a4a7ef762ef03a2abe5ef89efd727
rrty3550-prog <rrty3550@gmail.com>
2026-08-28 02:31:16 +0300

    fixing ability#1

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -1,50 +1,17 @@
 package example;
 
-import arc.*;
-import arc.util.*;
-import mindustry.game.EventType.*;
-import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
+import mindustry.content.UnitTypes;
+import mindustry.mod.Mod;
 
-public class ExampleJavaMod extends Mod{
+public class ExampleJavaMod extends Mod {
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
+    public ExampleJavaMod() {
+        // Конструктор мода
     }
 
     @Override
-    public void loadContent(){
-        package example;
-
-        import mindustry.content.UnitTypes;
-        import mindustry.mod.Mod;
-
-        public class ExampleJavaMod extends Mod {
-
-         public ExampleJavaMod() {
-              // Конструктор мода
-         }
-
-         @Override
-         public void loadContent() {
-             // Добавляем нашу способность ванильному юниту
-                UnitTypes.antumbra.abilities.add(new DodgeAbility());
-         }
-        }
-        Log.info("Loading some example content.");
+    public void loadContent() {
+        // Добавляем нашу способность ванильному юниту
+        UnitTypes.antumbra.abilities.add(new DodgeAbility());
     }
-
 }
